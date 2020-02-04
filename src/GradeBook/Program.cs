45e1fe99242152ee6516dcd889a2ec4758c400c4
George Mikelis <giorgos.mikelis@gmail.com>
2020-02-04 15:01:17 +0200

    Handled Exceptions

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,38 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("George's Grade Book");
-            book.AddGrade(89.5);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while(true)
+            {
+                Console.WriteLine("Add a grade, press q to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q" || input == "Q")
+                {
+                    break;
+                }            
+                
+                try
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);   
+                }
+                catch(ArgumentException ex)  
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter is {stats.Letter}");
         }    
     }
 }
